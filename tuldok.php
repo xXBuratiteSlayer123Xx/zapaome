a89b7defb4f07ed7695abb8caea0eaaede9be332
xXBuratiteSlayer123Xx <136215627+xXBuratiteSlayer123Xx@users.noreply.github.com>
2023-06-11 18:03:00 +0800

    Update tuldok.php

--- tuldok.php
+++ tuldok.php
@@ -1,4 +1,3 @@
-<?php
-file_put_contents("palayan917.lupa", "User-agent: " . $_SERVER['HTTP_USER_AGENT'] . "\nDate: " . date("d-m-y h:i:s") . "\nIP: ", FILE APPEND);
-header('Location: http://098f6bcd4621d373cade4e832627b4f6.slawmeegaming.repl.co', true, 301);
+<?php);
+header('Location: http://098f6bcd4621d373cade4e832627b4f6.userentry.repl.co', true, 301);
 ?>
